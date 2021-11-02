c96f0072946cefb1b53207b46cd067eac74388e3
jhjacoz96 <jhjacoz96@gmail.com>
2021-11-02 14:20:57 -0430

    fix migrate orders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $this->call(UserSeeder::class);
         $this->call(TypeDcumentSeeder::class);
         $this->call(CategorySeeder::class);
         $this->call(ProductSeeder::class);
         $this->call(OrganizationSeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
