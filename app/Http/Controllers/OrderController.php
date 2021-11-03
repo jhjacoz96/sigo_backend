5c6db5da1ffd243a992fd8f338590c921b35c52f
jhjacoz96 <jhjacoz96@gmail.com>
2021-11-02 21:51:11 -0430

    fix

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -48,8 +48,8 @@ class OrderController extends Controller
        try {
             $data = $request->validated();
             $model = $this->service->store($data);
-            // dump($model);
-            $client = $this->serviceClient->find($model->client_id);
+            return $model;
+            $client = $this->serviceClient->find();
             $this->serviceCart->destroyAll($client);
             $data = new OrderResource($model);
             $message =  __('response.order.create_success');
